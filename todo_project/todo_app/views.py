6ab64111b7968d4af1d59c538d12cea6afa4f724
ali <goli3148@gmail.com>
2023-03-16 02:14:30 +0330

    require http methods

--- todo_project/todo_app/views.py
+++ todo_project/todo_app/views.py
@@ -3,11 +3,12 @@ from todo_app.models import todo
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from datetime import datetime
+from django.views.decorators.http import require_http_methods
 
 def index(request):
     list = todo.objects.all().values()
     return render(request, 'index.html', {'todo_list':list})
-    
+   
 def insert(request):
     return render(request, 'insert.html')
     
@@ -28,14 +29,14 @@ def update(request,id):
         date = datetime.strftime(todo_up.date, '%m/%d/%Y %I:%M %p')
         return render(request, 'update.html', {'todo':todo_up, 'date':date})
 
-
+@require_http_methods(['POST']) # updateDB view is working only if POST request is available
 def updateDB(request, id):
     todo_up = get_object_or_404(todo, pk=id)
     todo_up.subject = request.POST['Subject']
     todo_up.extra = request.POST['extra']
     # request.POST['date'] => 03/09/2023 9:17 AM
     date = request.POST['date']
-    if date is not None or date == '':
+    if date is not None and not date == '':
         todo_up.date = datetime.strptime(date, "%m/%d/%Y %I:%M %p") 
     todo_up.save()
     return HttpResponseRedirect(reverse('todo_app:index'))
