64095e1a5c51022b06be7f4ba4eb04db1bd0c327
ali <goli3148@gmail.com>
2023-03-19 12:48:41 +0330

    update & insert EDITED

--- todo_project/todo_app/urls.py
+++ todo_project/todo_app/urls.py
@@ -4,8 +4,6 @@ app_name = 'todo_app'
 urlpatterns = [
     path('', views.index, name='index'),
     path('insert', views.insert, name='insert'),
-    path('insertDB', views.insertDB, name='insertDB'),
     path('update/<int:id>', views.update, name='update'),
-    path('updateDB/<int:id>', views.updateDB, name='updateDB'),
     path('deleteDB/<int:id>', views.deleteDB, name='deleteDB'),
 ]
--- todo_project/todo_app/views.py
+++ todo_project/todo_app/views.py
@@ -10,36 +10,34 @@ def index(request):
     return render(request, 'index.html', {'todo_list':list})
    
 def insert(request):
-    return render(request, 'insert.html')
-    
-def insertDB(request):
-    sub = request.POST['Subject']
-    ext = request.POST['extra']
-    # request.POST['date'] => 03/09/2023 9:17 AM
-    dat = datetime.strptime(request.POST['date'], "%m/%d/%Y %I:%M %p") 
-    todo(subject=sub, extra=ext, date=dat).save()
-    return HttpResponseRedirect(reverse('todo_app:index'))
+    if request.method == "GET":
+        return render(request, 'insert.html')
+    elif request.method == "POST":
+        sub = request.POST['Subject']
+        ext = request.POST['extra']
+        # request.POST['date'] => 03/09/2023 9:17 AM
+        dat = datetime.strptime(request.POST['date'], "%m/%d/%Y %I:%M %p") 
+        todo(subject=sub, extra=ext, date=dat).save()
+        return HttpResponseRedirect(reverse('todo_app:index'))
 
 def update(request,id):
     todo_up = get_object_or_404(todo, pk=id)
-    # todo_up.date => March 9, 2023, 9:17 a.m.
-    if (todo_up.date == None):
-        return render(request, 'update.html', {'todo':todo_up, 'date':None})
-    else:
-        date = datetime.strftime(todo_up.date, '%m/%d/%Y %I:%M %p')
-        return render(request, 'update.html', {'todo':todo_up, 'date':date})
-
-@require_http_methods(['POST']) # updateDB view is working only if POST request is available
-def updateDB(request, id):
-    todo_up = get_object_or_404(todo, pk=id)
-    todo_up.subject = request.POST['Subject']
-    todo_up.extra = request.POST['extra']
-    # request.POST['date'] => 03/09/2023 9:17 AM
-    date = request.POST['date']
-    if date is not None and not date == '':
-        todo_up.date = datetime.strptime(date, "%m/%d/%Y %I:%M %p") 
-    todo_up.save()
-    return HttpResponseRedirect(reverse('todo_app:index'))
+    if request.method == "GET":
+        # todo_up.date => March 9, 2023, 9:17 a.m.
+        if (todo_up.date == None):
+            return render(request, 'update.html', {'todo':todo_up, 'date':None})
+        else:
+            date = datetime.strftime(todo_up.date, '%m/%d/%Y %I:%M %p')
+            return render(request, 'update.html', {'todo':todo_up, 'date':date})
+    elif request.method == "POST":
+        todo_up.subject = request.POST['Subject']
+        todo_up.extra = request.POST['extra']
+        # request.POST['date'] => 03/09/2023 9:17 AM
+        date = request.POST['date']
+        if date is not None and not date == '':
+            todo_up.date = datetime.strptime(date, "%m/%d/%Y %I:%M %p") 
+        todo_up.save()
+        return HttpResponseRedirect(reverse('todo_app:index'))
 
 def deleteDB(request, id):
     todo_del = get_object_or_404(todo, pk=id)
